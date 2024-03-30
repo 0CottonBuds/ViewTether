929ffbca182e2b9c37b9ff00a4f6ca50f5b301b7
unknown <emiljohnllanes772@gmail.com>
2024-03-30 11:44:01 +0800

    finish testing signals and slots

--- ScreenCapture/App.cpp
+++ ScreenCapture/App.cpp
@@ -1,13 +1,19 @@
 #include "App.h"
 #include <QtWidgets/QWidget>
 #include <QtWidgets/QApplication>
+#include <QObject>
 
 App::App()
 {
-	this->screenDuplicator.Initialize();
+	screenDuplicator.Initialize();
+	mainWidget = new Ui::MainWidget();
 }
 
 App::~App()
+{
+}
+
+App::App(const App&)
 {
 }
 
@@ -15,8 +21,13 @@ void App::exec_ui(int argc, char **argv)
 {
 	QApplication app(argc, argv);
 	QWidget* widget = new QWidget();
-	mainWidget.setupUi(widget);
+	mainWidget->setupUi(widget);
+	QObject::connect(mainWidget->pushButton, SIGNAL(clicked()), this, SLOT(test()));
 
 	widget->show();
 	app.exec();
+}
+
+void App::test() {
+	cout << "you pressed a button" << endl;
 }
--- ScreenCapture/App.h
+++ ScreenCapture/App.h
@@ -1,14 +1,20 @@
 #pragma once
 #include "ScreenDupliactor.h"
 #include "ui_MainWindow.h"
+#include <QObject>
+#include <iostream>
 
-class App {
+class App: QObject{
+	Q_OBJECT;
 public:
 	App();
+	App(const App&);
 	~App();
 	void exec_ui(int argc, char** argv);
 
+public slots:
+	void test();
 private:
 	ScreenDuplicator screenDuplicator;
-	Ui::MainWidget mainWidget;
+	Ui::MainWidget* mainWidget;
 };
