57d75a743ca7cef1fea53bedbb1f21f194a2a087
unknown <emiljohnllanes772@gmail.com>
2024-04-20 22:25:53 +0800

    tried to add client server communication didnt work will try again tom

--- ScreenCapture/DisplayStreamServer.cpp
+++ ScreenCapture/DisplayStreamServer.cpp
@@ -23,12 +23,27 @@ DisplayStreamServer::DisplayStreamServer(QObject* parent) : QObject(parent)
 void DisplayStreamServer::newConnection()
 {
     // need to grab the socket
-    QTcpSocket *socket = server->nextPendingConnection();
+    QTcpSocket* socket = server->nextPendingConnection();
 
     socket->write("Hello client\r\n");
-    socket->flush();
+    socket->write("How are you doin I am you are connected to the local host server");
 
     socket->waitForBytesWritten(3000);
+
+    socket->waitForReadyRead();
+
+    char* data = nullptr;
+    qint64 responseCode = socket->read(data, 30000);
+
+    if (responseCode == -1) {
+        qDebug() << "An error has occurred" << endl;
+    }
+    else if (data == nullptr) {
+        qDebug() << "User didn't give any data back" << endl;
+    }
+    else {
+        qDebug() << "User response: " << data << endl;
+    }
 
     socket->close();
 }
--- ScreenCapture/main.cpp
+++ ScreenCapture/main.cpp
@@ -6,10 +6,11 @@ using namespace Ui;
 
 int main(int argc, char **argv)
 {
-	App app = App(argc, argv);
-	//QCoreApplication app(argc, argv);
+	//App app = App(argc, argv);
+	QCoreApplication app(argc, argv);
+	DisplayStreamServer server = DisplayStreamServer();
+	app.exec();
 
-	//DisplayStreamServer server = DisplayStreamServer();
 	return 0;
 }
 
