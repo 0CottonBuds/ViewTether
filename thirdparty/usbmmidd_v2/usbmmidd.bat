24fe2d63fc8cd97171a3bab7b32a038dd8bdaf68
0CottonBuds <emiljohnllanes772@gmail.com>
2024-11-17 12:40:40 +0800

    update amyuni drivers

--- thirdparty/usbmmidd_v2/usbmmidd.bat
+++ thirdparty/usbmmidd_v2/usbmmidd.bat
@@ -5,10 +5,12 @@
 
 :AMD64
 @cmd /c deviceinstaller64.exe install usbmmidd.inf usbmmidd
+@cmd /c deviceinstaller64.exe enableidd 1
 @goto end
 
 :x86
 @cmd /c deviceinstaller.exe install usbmmidd.inf usbmmidd
-:end
+@cmd /c deviceinstaller.exe enableidd 1
 
+:end
 @pause
